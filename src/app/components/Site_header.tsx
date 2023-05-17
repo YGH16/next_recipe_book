4242fcd5f9b968beb6789f18f4a727dae25d2b68
Sobhan Akhtar <sobhanakhtar16@gmail.com>
2023-05-17 10:49:18 +0100

    Created a component to encompass the site title and subtitle

---
+++ src/app/components/Site_header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Site_header() {
+  return (
+    <div className='site_header mt-5 ml-5'>
+        <h1 className='landing_title pb-0.5 font-bold text-xl'>The Recipe Book</h1>
+        <h2 className='landing_subtitle font-light'>Your One Stop for sharing Recipes</h2>
+    </div>
+  )
+}
