c159b01f50f413b1a7c63e9b55ef020373cf3a90
Sobhan Akhtar <sobhanakhtar16@gmail.com>
2023-05-24 10:23:23 +0100

    setup state to store email and passwords

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,9 +1,14 @@
+'use client'
+import { useState } from "react";
 export default function Login() {
+  const [username, SetUsername] = useState('');
+  const [password, SetPassword] = useState('');
+  
   return (
     <div className="login_form text-center mt-10">
-        <input type="email" name="email_address" placeholder="Email" className="m-5 p-2" />
+        <input type="email" name="email_address" placeholder="Email" className="m-5 p-2" onChange={ e => SetUsername(e.target.value)} />
         <br />
-        <input type="password" name="password" placeholder="Password" className="m-5 p-2" />
+        <input type="password" name="password" placeholder="Password" className="m-5 p-2" onChange={ e=> SetPassword(e.target.value)} />
         <br />
         <button type="submit" className="m-5 pl-5 pr-5 pt-2 pb-2 border-none rounded-full bg-slate-400 ">Submit</button>
     </div>
