d5d18aab1bcbdc57cfab47bcbc8f775d85fa3be2
Sobhan Akhtar <sobhanakhtar16@gmail.com>
2023-05-24 10:24:28 +0100

    setup state for the signup fields

--- src/app/components/Signup.tsx
+++ src/app/components/Signup.tsx
@@ -2,18 +2,20 @@
 import { useState } from "react";
 
 export default function Signup() {
-    const [setUsername, username] = useState();
-    const [setPassword, password] = useState();
-    const [setEmail, email] = useState('');
-  return (
+    const [username, SetUsername] = useState('');
+    const [password, SetPassword] = useState('');
+    const [email, SetEmail] = useState('');
+  
+    return (
     <div className='signup_form text-center mt-10'>
-        <input type="email" name="email_address" placeholder='Email Address' className='m-5 p-2'/>
+        <input type="email" name="email_address" placeholder='Email Address' className='m-5 p-2' onChange={ e => SetEmail(e.target.value)}/>
         <br />
-        <input type="text" name="username" placeholder='Username' className='m-5 p-2' />
+        <input type="text" name="username" placeholder='Username' className='m-5 p-2' onChange={ e => SetUsername(e.target.value)}/>
         <br />
-        <input type="password" name="password" placeholder='Password' className='m-5 p-2' />
+        <input type="password" name="password" placeholder='Password' className='m-5 p-2' onChange={ e => SetPassword(e.target.value)}/>
         <br />
         <button type="submit" className='m-5 pl-5 pr-5 pt-2 pb-2 border-none rounded-full bg-slate-400'>Submit</button>
+
     </div>
   )
 }
